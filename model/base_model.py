f7b842acb60a5f4ae52de596c20162d35e6352ee
run time error <quanghung20gg@gmail.com>
2024-08-21 15:24:40 +0700

    encode_text for baseline

--- model/base_model.py
+++ model/base_model.py
@@ -35,6 +35,14 @@ class BaselineCLIP(CLIP):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.to(self.device)
         
+    def encode_text(self, text, result = 'eos'):
+        inputs = self.tokenizer(text, max_length=self.max_length, padding=True, truncation=True, return_tensors='pt').to(self.device)
+    
+        emb_text = self.text_model(**inputs)
+
+        emb_norm = torch.norm(emb_text, dim=1, keepdim=True)
+        return emb_text / (emb_norm + 1e-8)
+        
 class BaselineSigLIP(BaselineCLIP):
     """
         Implementation of the Baseline SigLIP model.
