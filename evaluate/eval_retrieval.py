1fb1e9c08f05210febdf2caab63efc4dcb70a883
run time error <quanghung20gg@gmail.com>
2024-08-21 14:05:43 +0700

    torch -> numpy

--- evaluate/eval_retrieval.py
+++ evaluate/eval_retrieval.py
@@ -89,9 +89,11 @@ class EvaluateModel:
         self.dataset = get_dataset(eval_args['dataset'])
         
     def _encode_text(self, texts):
+        
+        # Encode text by batch
         for i in range(0, len(texts), self.eval_args['batch_size']):
             text_batch = texts[i:i+self.eval_args['batch_size']]
-            text_batch = self.model.encode_text(text_batch)
+            text_batch = self.model.encode_text(text_batch).cpu().numpy() # (batch, embedding_dim), convert to numpy
             if i == 0:
                 text_embeddings = text_batch
             else:
@@ -103,10 +105,14 @@ class EvaluateModel:
         return Image.open(image_path)
     
     def _encode_image(self, images):
+        
+        # Encode image by batch
         for i in range(0, len(images), self.eval_args['batch_size']):
             image_batch = images[i:i+self.eval_args['batch_size']]
+            
+            # Open image
             image_batch = [self.open_image(image) for image in image_batch]
-            image_batch = self.model.encode_image(image_batch)
+            image_batch = self.model.encode_image(image_batch).cpu().numpy() # (batch, embedding_dim), convert to numpy
             if i == 0:
                 image_embeddings = image_batch
             else:
