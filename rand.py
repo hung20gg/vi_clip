6c8202bed9e964b8c757adb2620f3cd4a239bb93
run time error <quanghung20gg@gmail.com>
2024-08-24 18:20:56 +0700

    rand

--- rand.py
+++ rand.py
@@ -1,11 +1,15 @@
 import torch
 import torch.distributed as dist
 import torch.nn as nn
+import os
 from torch.nn.parallel import DistributedDataParallel as DDP
 
 # Initialize process group
 def setup_ddp(rank, world_size):
-    dist.init_process_group(backend="nccl", init_method="env://", world_size=world_size, rank=rank)
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "54321"
+    
+    dist.init_process_group(backend="nccl", world_size=world_size, rank=rank)
     torch.cuda.set_device(rank)
 
 # Contrastive loss function (example implementation)
