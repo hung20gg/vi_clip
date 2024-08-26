9290fe51a4a0691938a30e7a18a1deb3a4b023c7
run time error <quanghung20gg@gmail.com>
2024-08-26 15:02:00 +0700

    ...

--- rand.py
+++ rand.py
@@ -36,7 +36,7 @@ def contrastive_loss(g_image_embeds, g_text_embeds, image_embeds, text_embeds):
 # Example CLIP model (simplified)
 
 def sigliploss(image_embed, text_embed, logit_scale = 1.0, logit_bias = 0.0, ddp=False, all_gather = True):
-    
+    print(f"DDP: {ddp}, All gather: {all_gather}")
     if ddp == False and all_gather == True:
         raise ValueError("All gather is only available in DDP mode")
     
@@ -85,7 +85,7 @@ def sigliploss(image_embed, text_embed, logit_scale = 1.0, logit_bias = 0.0, ddp
         # Go through all processes and get the image embed
         
         print(f"World size in forward model: {world_size}")
-        print(not all_gather, f"Rank {rank} received image embed from rank {rank}, loss: ", loss)
+        print(all_gather, f"Rank {rank} received image embed from rank {rank}, loss: ", loss)
         torch.distributed.broadcast(image_embed, rank)
         
         for i in range(world_size):
@@ -102,7 +102,7 @@ def sigliploss(image_embed, text_embed, logit_scale = 1.0, logit_bias = 0.0, ddp
                 nll = - torch.sum(loglik)
                 loss += torch.mean(nll)
                 
-                print(not all_gather, f"Rank {rank} received image embed from rank {i}, loss: ", torch.mean(nll))
+                print(all_gather, f"Rank {rank} received image embed from rank {i}, loss: ", torch.mean(nll))
                 # print('2 neighbors', (image_embed + neighbor_image_embed)[:,:10])
                 
             # else: # Send the image embed to other processes
@@ -199,7 +199,7 @@ def train(rank, world_size, model, images, texts, all_gather=False, loss_ = 'sig
     # loss.backward()
     # optimizer.step()
 
-    print(f"Rank {rank}, Loss: {loss.item()}")
+    print(f"Rank {rank}, Loss: {loss.sum().item()}")
 
 # Example usage
 if __name__ == "__main__":
